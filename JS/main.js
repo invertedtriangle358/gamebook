c2b0d202e91c29f7308341890dafd01fd54c3b3a
逆砂 参角 <135438665+invertedtriangle358@users.noreply.github.com>
2025-09-10 22:45:42 +0900

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+import { loadScenario, showScene } from "./scenario.js";
+import { connectRelays, loadMyLogs } from "./relay.js";
+
+let textEl, choicesEl, logEl;
+
+async function startGame() {
+  textEl = document.getElementById("text");
+  choicesEl = document.getElementById("choices");
+  logEl = document.getElementById("log");
+
+  await connectRelays(logEl);
+  await loadScenario(logEl);
+  showScene("start", textEl, choicesEl, logEl);
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  await startGame();
+  loadMyLogs(logEl);
+});
