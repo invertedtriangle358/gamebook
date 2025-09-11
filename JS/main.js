6e6870feb42517f0adc90a6e8a0132516a75c2e4
逆砂 参角 <135438665+invertedtriangle358@users.noreply.github.com>
2025-09-11 23:26:43 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 import { loadScenario, showScene } from "./scenario.js";
 import { connectRelays, loadMyLogs } from "./relay.js";
-import { log } from "./JS/logger.js";
+import { log } from "./logger.js";
 
 let textEl, choicesEl, logEl;
 
